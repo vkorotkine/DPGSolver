01f26cf234939fedc421c6c0ba9002484811fa78
Philip <philip@McGillMac.local>
2018-06-06 08:11:31 -0400

    Made CMake run more system independent.

--- cmake/run/macOS_gcc_release.sh
+++ cmake/run/macOS_gcc_release.sh
@@ -1,26 +1,28 @@
 #!/bin/bash
 
-TOP_DIR="${PWD}/../.."
+DIR="${BASH_SOURCE%/*}"
+if [[ ! -d "$DIR" ]]; then DIR="$PWD"; fi
 
+PARAM_FILE="$DIR/parameters_release.sh"
+PARAM_EXAMPLE="$DIR/parameters_example.sh"
+if [ ! -f  ${PARAM_FILE} ]; then
+	echo "Parameter file (${PARAM_FILE}) not found."
+	echo "Please create it based on the example file (${PARAM_EXAMPLE})."
+else
+	. ${PARAM_FILE}
 
-# Modifiable parameters ****************************************************** #
+	TOP_DIR="${PWD}/../.."
+	BUILD_DIR=${TOP_DIR}/build
 
-export MKLROOT=/Users/philip/Desktop/research_codes/mkl/mkl_2018_2/mkl
-export PETSC_DIR=/Users/philip/Desktop/research_codes/petsc/petsc-3.9.2
-export PETSC_ARCH=arch-macOS-mkl-mpich-release
+	CMAKE_BUILD_TYPE=Release
+	TOOLCHAIN_FILE=gcc.cmake
 
-BUILD_DIR=${TOP_DIR}/build
-
-CMAKE_BUILD_TYPE=Release
-TOOLCHAIN_FILE=gcc.cmake
-
-# End Modifiable parameters ************************************************** #
-
-for dim in `seq 1 3`; do
-	BUILD_DIR_D=${BUILD_DIR}_${dim}D
-	mkdir -p ${BUILD_DIR_D} && cd ${BUILD_DIR_D}
-	cmake -D CMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE} \
-	      -D CMAKE_TOOLCHAIN_FILE=${TOP_DIR}/cmake/toolchains/${TOOLCHAIN_FILE} \
-	      -D BUILD_DIM=${dim} \
-	      ${TOP_DIR}
-done
+	for dim in `seq 1 3`; do
+		BUILD_DIR_D=${BUILD_DIR}_${dim}D
+		mkdir -p ${BUILD_DIR_D} && cd ${BUILD_DIR_D}
+		cmake -D CMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE} \
+			-D CMAKE_TOOLCHAIN_FILE=${TOP_DIR}/cmake/toolchains/${TOOLCHAIN_FILE} \
+			-D BUILD_DIM=${dim} \
+			${TOP_DIR}
+	done
+fi
---
+++ cmake/run/parameters_example.sh
@@ -0,0 +1,5 @@
+# Please make copies of this file called `parameters_[debug/release].sh` and use parameters specific to your system.
+
+export MKLROOT=/Users/philip/Desktop/research_codes/mkl/mkl_2018_2/mkl
+export PETSC_DIR=/Users/philip/Desktop/research_codes/petsc/petsc-3.9.2
+export PETSC_ARCH=arch-macOS-mkl-mpich-debug
